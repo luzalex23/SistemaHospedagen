1b423aaf95485156ae61759c057ff7c1609b911f
Iago Alves <iago20700@gmail.com>
2024-04-10 14:57:25 -0300

    DesafioReservaHotel

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,7 +17,7 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
@@ -25,6 +25,7 @@ namespace DesafioProjetoHospedagem.Models
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                throw new Exception($"Capacidade da Suite {Suite.TipoSuite} excedida, quantidade maxima é de {Suite.Capacidade}.");
             }
         }
 
@@ -37,24 +38,30 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
-        public decimal CalcularValorDiaria()
+        public string CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+           
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                decimal descontoPercentual = 0.10m;
+
+                valor = valor * (1 - descontoPercentual);
+
+                
             }
 
-            return valor;
+            return valor.ToString("C");
         }
     }
 }
